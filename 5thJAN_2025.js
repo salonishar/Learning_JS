14cdc53b059a7ee16136ade1a871731b1b54a988
Saloni Sharma <salonisha2007@gmail.com>
2026-01-06 01:09:44 +0530

    Strings and arrays

---
+++ 5thJAN_2025.js
@@ -0,0 +1,39 @@
+//--------------------------------STRINGS----------------------------------//
+// GENERATE A USERNAME STARTING WITH '@' FOLLOWED BY "FULL-NAME" AND LENGTH OF THE NAME //
+
+// let str = prompt("your name : ");
+// let length = str.length;
+
+// let username = `@${str}${length}`;
+// console.log(username);
+
+
+//--------------------------------ARRAYS----------------------------------//
+//FIND AVERAGE OF THE MARKS IN THE GIVEN ARRAY 
+
+// let marks = [85, 98, 44, 37, 60];
+
+// let totalMarks = 0;
+// for(let i=0; i<marks.length; i++){
+//     totalMarks += marks[i];
+// }
+
+// let average = totalMarks / marks.length;
+
+// console.log(average);
+
+
+//------------------------------------------------------------------------//
+//PRICE OF 5 ITEMS IS GIVEN , EVERY ITEM HAS 10% OFF, GIVE THE FINAL PRICE 
+
+// let price = [250, 645, 300, 900, 50];
+
+// for (let i=0; i<price.length; i++){
+//     let finalPrice = price[i] - (1/10)*price[i];
+//     price[i] = finalPrice;
+//     console.log(price[i]);
+// }
+
+
+//------------------------------------------------------------------------//
+//

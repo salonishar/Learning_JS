2df95e4af6be87e39aa04c69b8bf745e898db792
Saloni Sharma <salonisha2007@gmail.com>
2026-01-05 21:36:38 +0530

    Update : 3rd Jan practice

---
+++ 3rdJAN_2025.js
@@ -0,0 +1,27 @@
+// check if the number is a multiple of 5 or not //
+
+// let number = prompt("Enter a number : ");
+
+// if (number % 5 !== 0) console.log("NO");
+// else console.log("YES");
+
+// <==================== scores and grades: =========================>
+// 80 - 100 : A; 70 - 79 : B; 60 - 69 : C ; 50 - 59 : D ; 0 - 49 : F
+
+// let score = prompt("Enter your score : ");
+// let grade;
+
+// if (score >= 80 && score <= 100) grade = 'A';
+// else if (score >= 70 && score < 80) grade = 'B';
+// else if (score >= 60 && score < 70) grade = 'C';
+// else if (score >= 50 && score < 60) grade = 'D';
+// else if (score >= 0 && score < 50) grade = 'F';
+// else console.log("INVALID INPUT");
+
+// console.log("Grade : ", grade);
+
+// print all even numbers from 0 to 100
+
+for (let i = 0; i <= 100; i++){
+    console.log("i");
+}

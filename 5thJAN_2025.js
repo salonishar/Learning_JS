62659d587fc4e53b6fdfe857f467fabae61dfb57
Saloni Sharma <salonisha2007@gmail.com>
2026-01-06 15:47:44 +0530

    Functions & Mthods

--- 5thJAN_2025.js
+++ 5thJAN_2025.js
@@ -1,45 +1,46 @@
 //--------------------------------STRINGS----------------------------------//
 // GENERATE A USERNAME STARTING WITH '@' FOLLOWED BY "FULL-NAME" AND LENGTH OF THE NAME //
 
-// let str = prompt("your name : ");
-// let length = str.length;
+let str = prompt("your name : ");
+let length = str.length;
 
-// let username = `@${str}${length}`;
-// console.log(username);
+let username = `@${str}${length}`;
+console.log(username);
 
 
 //--------------------------------ARRAYS----------------------------------//
 //FIND AVERAGE OF THE MARKS IN THE GIVEN ARRAY 
 
-// let marks = [85, 98, 44, 37, 60];
+let marks = [85, 98, 44, 37, 60];
 
-// let totalMarks = 0;
-// for(let i=0; i<marks.length; i++){
-//     totalMarks += marks[i];
-// }
+let totalMarks = 0;
+for(let i=0; i<marks.length; i++){
+    totalMarks += marks[i];
+}
 
-// let average = totalMarks / marks.length;
+let average = totalMarks / marks.length;
 
-// console.log(average);
+console.log(average);
 
 
 //------------------------------------------------------------------------//
 //PRICE OF 5 ITEMS IS GIVEN , EVERY ITEM HAS 10% OFF, GIVE THE FINAL PRICE 
 
-// let price = [250, 645, 300, 900, 50];
+let price = [250, 645, 300, 900, 50];
 
-// for (let i=0; i<price.length; i++){
-//     let finalPrice = price[i] - (1/10)*price[i];
-//     price[i] = finalPrice;
-//     console.log(price[i]);
-// }
+for (let i=0; i<price.length; i++){
+    let finalPrice = price[i] - (1/10)*price[i];
+    price[i] = finalPrice;
+    console.log(price[i]);
+}
 
 
 //------------------------------------------------------------------------//
 // (a)REMOVE FIRST ELEMENT (b)REPLACE Uber WITH Ola (c)ADD Amazon IN LAST
-// let companies = ['bloomberg', 'Microsoft', "Uber", "Google", "IBM", "Netflix"];
 
-// companies.shift();
-// companies.splice(1, 1, "Ola");
-// companies.push("Amazon");
-// console.log(companies);
+let companies = ['bloomberg', 'Microsoft', "Uber", "Google", "IBM", "Netflix"];
+
+companies.shift();
+companies.splice(1, 1, "Ola");
+companies.push("Amazon");
+console.log(companies);
